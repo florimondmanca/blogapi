fe278925317014f910082321171a8f15df088c05
florimondmanca <florimond.manca@gmail.com>
2019-05-10 20:35:18 +0200

    add cors and allowed hosts settings

--- blogapi/settings.py
+++ blogapi/settings.py
@@ -2,6 +2,15 @@ from starlette.config import Config
 
 config = Config(".env")
 
+TESTING = False
+
 DATABASE_URL = config("DATABASE_URL")
 TEST_DATABASE_URL = config("TEST_DATABASE_URL")
-TESTING = False
+
+ALLOWED_HOSTS = ["127.0.0.1", "*.florimond.dev"]
+
+CORS = {
+    "allow_origins": ["http://localhost:4200", "https://blog.florimond.dev"],
+    "allow_methods": ["*"],
+    "allow_headers": ["*"],
+}
